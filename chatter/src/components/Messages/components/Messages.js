6781f4deb426cd27cff18b00e050a72d536047f1
ROHAN-WAGH-ROOT <rohanwagh234@gmail.com>
2021-10-15 14:42:14 +0530

    some changes with initial msg of botty

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import useSound from 'use-sound';
 import config from '../../../config';
@@ -8,25 +8,69 @@ import Header from './Header';
 import Footer from './Footer';
 import Message from './Message';
 import '../styles/_messages.scss';
+import initialBottyMessage from '../../../common/constants/initialBottyMessage';
 
-const socket = io(
-  config.BOT_SERVER_ENDPOINT,
-  { transports: ['websocket', 'polling', 'flashsocket'] }
-);
+const socket = io.connect('http://localhost:4001', { transports: [ 'websocket', 'polling', 'flashsocket' ] });
+console.log(socket);
 
-function Messages() {
-  const [playSend] = useSound(config.SEND_AUDIO_URL);
-  const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
-  const { setLatestMessage } = useContext(LatestMessagesContext);
+// socket.on('bot-message', (message) => {
+// 	socket.emit('bot-message', message);
+// 	console.log(message);
 
-  return (
-    <div className="messages">
-      <Header />
-      <div className="messages__list" id="message-list">
-      </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
-    </div>
-  );
+socket.on('connect', () => {
+	console.log(socket.connected);
+});
+socket.on('disconnect', () => {
+	console.log(socket.connected);
+});
+function Messages(props) {
+	const [ message, setMessage ] = useState('');
+	const [ chat, setChat ] = useState([]);
+	const [ playSend ] = useSound(config.SEND_AUDIO_URL);
+	const [ playReceive ] = useSound(config.RECEIVE_AUDIO_URL);
+	const { setLatestMessage } = useContext(LatestMessagesContext);
+	useEffect(() => {
+		socket.on('chat', (message) => {
+			setChat([ ...chat, message ]);
+		});
+	});
+	const renderChat = () => {
+		return (
+			<div>
+				<div>{initialBottyMessage}</div>
+				{chat.map((message, index) => {
+					return <p key={index}>{message}</p>;
+				})}
+			</div>
+		);
+	};
+	const sendMessage = (e) => {
+		e.preventDefault();
+		socket.on('bot-message', (message) => {
+			socket.emit('bot-message', message);
+			setMessage('');
+		});
+		playSend();
+	};
+
+	const onChangeMessage = (e) => {
+		setMessage(...message, e.target.value);
+	};
+	const receiveMessage = () => {
+		playReceive();
+	};
+	return (
+		<div className="messages">
+			<Header />
+			<div className="messages__list" id="message-list">
+				{renderChat()}
+			</div>
+			<div className="messages__message messages__message--me" id="messages__message--me">
+				{message}
+			</div>
+			<Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
+		</div>
+	);
 }
 
 export default Messages;
